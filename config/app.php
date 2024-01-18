1909e4be1b6a8cf31d3b83df092dad285775b778
vinicius <vinicius261202@gmail.com>
2024-01-18 11:04:40 -0300

    chore: docker/nginx estrutura

--- config/app.php
+++ config/app.php
@@ -55,7 +55,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://mp.balestrin.dev'),
 
     'asset_url' => env('ASSET_URL'),
 
@@ -168,6 +168,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        App\Providers\RepositoryServiceProvider::class
     ])->toArray(),
 
     /*
---
+++ config/mercadopago.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'acess_token' => env("MERCADOPAGO_ACESS_TOKEN"),
+    'public_key'  => env("MERCADOPAGO_PUBLIC_KEY"),
+    "integrador_id" => env("MERCADOPAGO_INTEGRATOR_ID"),
+    "email" => env("MERCADOPAGO_EMAIL")
+];
